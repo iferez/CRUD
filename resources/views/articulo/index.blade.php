99da1c97bdd4d8b3bb0f2f4b00b86e986e1c3ecb
Ivan Ferez <ivanferez.web@gmail.com>
2022-04-01 16:58:02 -0300

    Agregado de DataTables

--- resources/views/articulo/index.blade.php
+++ resources/views/articulo/index.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.plantillabase')
 
+
+@section('css')
+<link href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+@endsection
+
 @section('contenido')
 
-<a href="articulos/create" class="btn btn-primary">CREAR</a>
+<a href="articulos/create" class="btn btn-primary mb-5">CREAR</a>
 
-<table class="table table-dark table-striped table-hover my-5">
-    <thead>
+<table id="articulosTable" class="table table-striped table-hover shadow-sm my-5">
+    <thead class="bg-primary text-white">
         <tr>
             <th scope="col">ID</th>
             <th scope="col">Código</th>
@@ -36,5 +41,19 @@
         @endforeach
     </tbody>
 </table>
+
+@section('js')
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
+
+<script>
+    $(document).ready(function() {
+    $('#articulosTable').DataTable({
+        "lengthMenu": [[5,10,50,-1], [5,10,50,"All"]]
+    });
+} );
+</script>
+@endsection
 
 @endsection
--- resources/views/layouts/plantillabase.blade.php
+++ resources/views/layouts/plantillabase.blade.php
@@ -5,13 +5,14 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    @yield('css')
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>CRUD</title>
   </head>
   <body>
-      <h1 class="bg-warning text-white text-center py-3">CRUD con Laravel y Bootstrap 5</h1>
+      <h1 class="bg-black text-white text-center py-3">CRUD con Laravel y Bootstrap 5</h1>
 
 <div class="container">
     @yield('contenido')
@@ -20,5 +21,6 @@
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
+    @yield('js')
   </body>
 </html>
